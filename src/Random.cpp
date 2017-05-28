176adbf6ca750e5c64657f7af2354e1b2d6611ef
Santiago Lizardo <santiago.lizardo@gmail.com>
2017-05-28 20:35:53 +0100

    Random class reformarted

--- src/Random.cpp
+++ src/Random.cpp
@@ -10,51 +10,50 @@ using std::random_shuffle;
 bool Random::started = false;
 
 int Random::nextInt(int min, int max) {
-	if (started == false) {
-		srand(time(0));
-		started = true;
-	}
+    if (started == false) {
+        srand(time(0));
+        started = true;
+    }
 
-	//return (min + (rand()%(min+1)));
-	return min + (int) ((float) (max-min+1) * (rand() / (RAND_MAX + 1.0)));
+    return min + (rand() % (max - min + 1));
 }
 
 int* Random::nextArray(vector<int> list, unsigned int limit) {
-	int* result = new int[limit];
+    int* result = new int[limit];
 
-	srand(time(nullptr));
-	random_shuffle(list.begin(), list.end());
+    srand(time(nullptr));
+    random_shuffle(list.begin(), list.end());
 
-	for (unsigned int i = 0; i < limit; i++) {
-		result[i] = list[i];
-	}
+    for (unsigned int i = 0; i < limit; i++) {
+        result[i] = list[i];
+    }
 
-	return result;
+    return result;
 }
 
 int* Random::nextArray(int* list, int size, unsigned int limit) {
-	int* result = new int[limit];
+    int* result = new int[limit];
 
-	srand(time(nullptr));
-	random_shuffle(list, list + size);
+    srand(time(nullptr));
+    random_shuffle(list, list + size);
 
-	for (unsigned int i = 0; i < limit; i++) {
-		result[i] = list[i];
-	}
+    for (unsigned int i = 0; i < limit; i++) {
+        result[i] = list[i];
+    }
 
-	return result;
+    return result;
 }
 
 vector<int> Random::nextVector(vector<int> list, unsigned int limit) {
-	vector<int> result(limit);
+    vector<int> result(limit);
 
-	srand(time(nullptr));
-	random_shuffle(list.begin(), list.end());
+    srand(time(nullptr));
+    random_shuffle(list.begin(), list.end());
 
-	for (unsigned int i = 0; i < limit; i++) {
-		result[i] = list[i];
-	}
+    for (unsigned int i = 0; i < limit; i++) {
+        result[i] = list[i];
+    }
 
-	return result;
+    return result;
 }
 
